df4638495b1348eedf20e4eb0cba180b8365b8fb
DaniMMozz <danimomon23@gmail.com>
2026-07-24 17:20:04 -0500

    Add files via upload
    Implementación del módulo de Modelo de Dominio para el proyecto final de POO, definiendo las entidades base Cliente (con herencia para ClienteNatural y ClienteEmpresarial), Producto, PedidoItem y Pedido (con herencia para PedidoNacional y PedidoInternacional), junto con sus respectivas reglas de negocio, encapsulamiento defensivo, validaciones en setters e implementación de herencia y polimorfismo.

---
+++ CLIENTEeHIJAS.cs
@@ -0,0 +1,117 @@
+// Proyecto Final POO Clase Padre e Hijas: Cliente , ClienteNatural y ClienteEmpresarial
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace EcommercePipeline.Dominio{
+    public abstract class Cliente
+    {   
+        private const string PatronEmail = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+        // Explicación del patrón (Por Daniel M):
+
+        //Nota: El @ antes del string sirve para indicarle a C# que interprete las barras invertidas \ de forma literal sin escapar caracteres.
+
+        // ^[^@\s]+  -> Inicia con uno o más caracteres que NO son '@' ni espacios.
+
+        // @  -> Obliga a tener exactamente un símbolo '@'.
+
+        // [^@\s]+  -> Seguido del nombre del dominio (ej: gmail, hotmail, unimagdalena).
+
+        // \.  -> Obliga a tener al menos un punto '.'.
+
+        // [^@\s]+$  -> Termina con la extensión del dominio (ej: com, edu.co, org, net).
+
+        //Explicación de porque hago esto: En el enunciado del proyecto final se nos informo que solo se permiten correos validos, yo supuse, uno que pertenezca a un dominio aceptable, como el comun "email.com", pero ya que exiten mas dominios que si podrian ser aceptados si cumplen con el orden adecuado, investigando cree este patron que debe tener todo correo que pueda ser aceptado.
+        private string _id;
+        private string _nombre;
+
+        private string _email;
+
+        private string _ciudad;
+
+        protected Cliente (string id , string nombre , string email , string ciudad )
+        {
+            ID = id;
+            Nombre = nombre;
+            Email = email;
+            _ciudad = ciudad;
+        }
+
+        public string ID
+        {
+            get {return _id;}
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("La ID no puede ser nula o estar vacia");
+                }
+                _id = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get {return _nombre;}
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("El nombre no puede ser nulo o estar vacio");
+                }
+                _nombre = value;
+            }
+        } 
+
+        public string Email
+        {
+            get {return _email;}
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("El email no puede estar vacio");
+                }
+                else if (!Regex.IsMatch(value, PatronEmail))
+                {
+                    throw new ArgumentException($"El formato del email '{value}' no es un correo electronico valido");
+                }
+                _email = value;
+            }
+        } 
+
+        public string Ciudad
+        {
+            get{return _ciudad;}
+            set{_ciudad = value;}
+        }   
+
+        public abstract bool EsFrecuente(int cantidadCompras, decimal totalInvertido);  
+    }
+
+    public class ClienteNatural : Cliente
+    {
+        public ClienteNatural(string id , string nombre , string email , string ciudad) : base(id , nombre , email , ciudad)
+        {
+            
+        }
+
+        public override bool EsFrecuente(int cantidadCompras, decimal totalInvertido)
+        {
+            return cantidadCompras > 5;
+        }
+    }
+
+    public class ClienteEmpresarial : Cliente
+    {
+        public ClienteEmpresarial(string id , string nombre , string email , string ciudad) : base(id , nombre , email, ciudad)
+        {
+            
+        }
+
+        public override bool EsFrecuente(int cantidadCompras, decimal totalInvertido)
+        {
+            return totalInvertido > 50000000m;
+        }
+    }
+}
